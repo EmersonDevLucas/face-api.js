4aace96ff1a93bd15f11d67205242eba9cce06d5
Emerson Souza <166417132+EmersonDevLucas@users.noreply.github.com>
2025-05-09 14:01:05 -0300

    Criação de novas paginas

---
+++ pages/browser/public/js/face.js
@@ -0,0 +1,60 @@
+let forwardTimes = []
+let withBoxes = true
+
+function onChangeHideBoundingBoxes(e) {
+    withBoxes = !$(e.target).prop('checked')
+}
+
+function updateTimeStats(timeInMs) {
+    forwardTimes = [timeInMs].concat(forwardTimes).slice(0, 30)
+    const avgTimeInMs = forwardTimes.reduce((total, t) => total + t) / forwardTimes.length
+    $('#time').val(`${Math.round(avgTimeInMs)} ms`)
+    $('#fps').val(`${faceapi.utils.round(1000 / avgTimeInMs)}`)
+}
+
+async function onPlay() {
+    const videoEl = $('#inputVideo').get(0)
+
+    if (videoEl.paused || videoEl.ended || !isFaceDetectionModelLoaded())
+        return setTimeout(() => onPlay())
+
+
+    const options = getFaceDetectorOptions()
+
+    const ts = Date.now()
+
+    const result = await faceapi.detectSingleFace(videoEl, options).withFaceExpressions()
+
+    updateTimeStats(Date.now() - ts)
+
+    if (result) {
+        const canvas = $('#overlay').get(0)
+        const dims = faceapi.matchDimensions(canvas, videoEl, true)
+
+        const resizedResult = faceapi.resizeResults(result, dims)
+        const minConfidence = 0.05
+        if (withBoxes) {
+            faceapi.draw.drawDetections(canvas, resizedResult)
+        }
+        faceapi.draw.drawFaceExpressions(canvas, resizedResult, minConfidence)
+    }
+
+    setTimeout(() => onPlay())
+}
+
+async function run() {
+    await changeFaceDetector(TINY_FACE_DETECTOR)
+    await faceapi.loadFaceExpressionModel('/')
+    changeInputSize(224)
+
+    const stream = await navigator.mediaDevices.getUserMedia({ video: {} })
+    const videoEl = $('#inputVideo').get(0)
+    videoEl.srcObject = stream
+}
+
+function updateResults() { }
+
+$(document).ready(function () {
+    initFaceDetectionControls()
+    run()
+})
---
+++ pages/browser/public/js/home.js
@@ -0,0 +1,7 @@
+const btnYes = document.getElementById("yes");
+const btnNo = document.getElementById("no");
+
+btnYes.addEventListener("click", function () {
+    console.log("yes");
+    window.location.href = "/face";
+})
